a190bc640090f135b06bc8e105338d332ce653dd
Mohamad Moumneh <mmoumne0@email.cpcc.edu>
2021-11-02 10:36:59 -0400

    Imported fields and methods from Room class

--- AirConditioner.java
+++ AirConditioner.java
@@ -2,3 +2,72 @@
 
 public class AirConditioner
 {
+   //fields for our class 
+   private String Manufacturer;
+   private String Type; 
+   private int coolingCap; 
+   
+   //no-arg constructor 
+   public AirConditioner() 
+   {
+      Manufacturer = "";
+      Type = ""; 
+      coolingCap = 0; 
+   }
+   
+   //Constructor with arguments for manufacturer, type of unit, and cooling capacity 
+   public AirConditioner(String Manufacturer, String Type, int coolingCap) 
+   {
+      this.Manufacturer = Manufacturer; 
+      this.Type = Type; 
+      this.coolingCap = coolingCap; 
+   }
+   
+   //the setManufacturer method stores a manufacturer for the air conditioner 
+   public void setManufacturer(String Manufacturer)
+   {
+      this.Manufacturer = Manufacturer;
+   }
+   
+   //the setType method stores which type of unit the air conditioner will be
+   public void setType(String Type)
+   {
+      this.Type = Type;
+   }
+   
+   //the setCooling method will set a cooling capacity for the air conditioner 
+   public void setCooling(int coolingCap)
+   {
+      this.coolingCap = coolingCap;
+   }
+   
+   //the getManufacturer method will return the manufacturer for the unit
+   public String getManufacturer()
+   {
+      return Manufacturer;
+   }
+   
+   //the getType method will return the type of air conditioner 
+   public String getType()
+   {
+      return Type;
+   }
+   
+   //the getCooling method will return a cooling capacity for the unit
+   public int getCooling()
+   {
+      return coolingCap;
+   }
+   
+   //the toString method will return information about an air conditioner unit
+   public String toString()
+   {
+      String str = "The Air Conditioner Manufacturer: " + Manufacturer +
+                   "\nThe AC type: " + Type +
+                   "\nThe AC cooling capacity: " + coolingCap;
+      return str; 
+   }  
+}
+   
+   
+      
